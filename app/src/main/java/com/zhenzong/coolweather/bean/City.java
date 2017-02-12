be9bb26890e2e8b2e30bf819c255580f5b0c5af3
Zhenzong <reozong@gmail.com>
2017-02-12 10:01:17 +0800

    First commit.

---
+++ app/src/main/java/com/zhenzong/coolweather/bean/City.java
@@ -0,0 +1,11 @@
+package com.zhenzong.coolweather.bean;
+
+/**
+ * @Description:
+ *
+ * @By: zheozong on 2017/2/10 19:53
+ * @Email: reozong@gmail.com
+ * @Reference:
+ */
+public class City {
+}
---
+++ app/src/main/java/com/zhenzong/coolweather/constant/Global.java
@@ -0,0 +1,14 @@
+package com.zhenzong.coolweather.constant;
+
+/**
+ * @Description: 保存全局变量
+ *
+ * @By: zheozong on 2017/2/10 19:46
+ * @Email: reozong@gmail.com
+ * @Reference:
+ */
+public class Global {
+
+    /*获取中国所有的省份*/
+    public static final String CHINA_CITY = "http://guolin.tech/api/china";
+}
